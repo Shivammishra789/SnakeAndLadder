57372513b9510d50cced710c1c183da0f126af40
Shivammishra789 <shivammishra7789@gmail.com>
2021-10-03 15:16:01 +0530

    Check for ladder snake or no play

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,15 +1,37 @@
 
 public class SnakeAndLadder {
+
+	public static final int ladder=1;
+	public static final int snake=2;
 	
 	public static void main(String[] args) {
 	
 		int POSITION=0;
 
-		int dice=(int)Math.floor(Math.random() * 10)%6+1;
+		int dice=(int)Math.floor(Math.random() * 10)%6+1;  //to get random dice number
 
-		//Random dice number displayed
+                int options=(int)Math.floor(Math.random() * 10)%3;  //to get random 3 options to play
 
- 		System.out.println("Your dice number is " + dice);
+                System.out.println("Random dice number is " + dice);
+
+        switch(options) {
+
+        case ladder:
+                System.out.println("Ladder");
+                                POSITION+=dice;  //position is increased by random dice number
+                                break;
+        case snake:
+                System.out.println("Snake");
+                                POSITION-=dice;  //position is decresed by ramdom dice number
+                                break;
+        default:
+                System.out.println("No Play");   //position remains same
+
+                                break;
+
+                                        }
+        System.out.println("Players current position is " + POSITION );   //current position of player after play
+
 
 	}
 	
