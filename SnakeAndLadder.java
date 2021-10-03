434b6ac3815fed289e51cadc1ee4023a10e766a2
Shivammishra789 <shivammishra7789@gmail.com>
2021-10-03 14:05:50 +0530

    Start position zero

---
+++ SnakeAndLadder.java
@@ -0,0 +1,12 @@
+
+public class SnakeAndLadder {
+	
+	public static void main(String[] args) {
+	
+		int POSITION=0;
+
+		System.out.println("Current position of player is " + POSITION);
+	}
+	
+
+}
